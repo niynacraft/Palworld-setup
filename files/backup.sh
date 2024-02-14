dd02d71075621d133c7c5097b6dd943cd1b9f239
niynacraft <cat@catcraft.stars.ne.jp>
2024-02-14 17:17:55 +0900

    aa

--- files/backup.sh
+++ files/backup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# タイムスタンプを生成
 timestamp=$(date +"%Y%m%d_%H%M%S")
 
+# palworld-server.serviceが実行中かどうかをチェック
 if systemctl is-active --quiet palworld-server.service; then
     echo "palworld-server.service is running"
+    
+    # バックアップファイルを作成
     tar -czvf ~/GameBackupData/PalServer_Backup_$timestamp.tar.gz -C ~/Steam/steamapps/common/PalServer/Pal/Saved .
+
+    # GameBackupDataディレクトリに移動して古いバックアップを削除
+    cd ~/GameBackupData || exit
+    ls -t PalServer_Backup_*.tar.gz | tail -n +201 | xargs rm --
 else
     echo "palworld-server.service is not running"
 fi
