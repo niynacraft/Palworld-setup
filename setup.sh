235cb3664b8d5cd2f63ad04d09814fc310d7fee7
niynacraft <cat@catcraft.stars.ne.jp>
2024-02-14 15:58:23 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 mkdir ~/script
 mkdir ~/.steam/
 mkdir ~/.steam/sdk64/
+sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
+steamcmd +login anonymous +app_update 2394010 validate +quit
