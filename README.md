bad79f3ccab34715b6764429e4d0d0deec18ba56
niynacraft <cat@catcraft.stars.ne.jp>
2024-02-14 17:45:29 +0900

    test

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-aa
+# パルワールドUbuntuセットアップ
+自己責任でど～～～ぞ
+
+## セットアップ手順
+
+ホームディレクトリの位置で実行してください。
+
+- git のインストール`sudo apt install git -y`
+- セットアップFileのダウンロード`git clone https://github.com/niynacraft/Palworld-setup.git`
+- セットアップの実行`sh Palworld-setup/setup.sh`
+
+## backup間隔の調整
+`/etc/cron.d/PalServer`のcronファイルを編集
+#### 例1) 30分ごと(default)
+```
+*/30 * * * * <user-name> ~/script/backup.sh
+```
+#### 例2) １時間ごと
+```
+0 * * * * <user-name> ~/script/backup.sh
+``` 
+
+## バックアップファイルの世代数
+`~/script/backup.sh`の15行目を編集
+
+残したい世代数+1
+
+#### 例1) 200世代残したいとき(default)
+```
+ls -t PalServer_Backup_*.tar.gz | tail -n +201 | xargs rm --
+```
+
+#### 例 2) 100世代残したいとき
+```
+ls -t PalServer_Backup_*.tar.gz | tail -n +101 | xargs rm --
+```
--- setup.sh
+++ setup.sh
@@ -6,9 +6,10 @@ mkdir ~/script
 mkdir ~/GameBackupData
 mkdir ~/.steam/
 mkdir ~/.steam/sdk64/
+echo "steamcmdのセットアップを開始します"
 sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
+echo "パルワのインスコを開始します"
 steamcmd +login anonymous +app_update 2394010 validate +quit
-
 cp -r ~/Steam/steamapps/common/PalServer/linux64/steamclient.so ~/.steam/sdk64/
 
 #スクリプトファイルのコピー
@@ -20,16 +21,18 @@ sudo chmod 700 ~/script/*
 #サービス化
 service_file="$home_directory/Palworld-setup/files/palworld-server.service"
 
-echo "Fileの書き換え"
+echo "使用ユーザーアカウントの調整"
 sed -i "s/<user-name>/$current_user/g" "$home_directory/Palworld-setup/files/palworld-server.service"
 sed -i "s/<user-name>/$current_user/g" "$home_directory/Palworld-setup/files/PalServer"
 
-echo "Fileのコピー"
+echo "ファイルの配置"
 
 sudo cp ~/Palworld-setup/files/palworld-server.service /etc/systemd/system/
 sudo cp ~/Palworld-setup/files/PalServer /etc/cron.d/
 
+echo "サービスの開始"
 sudo systemctl daemon-reload
 sudo systemctl enable palworld-server.service
 sudo systemctl start palworld-server.service
-sudo systemctl status palworld-server.service
+
+echo "せっとあっぷおわったよ"
