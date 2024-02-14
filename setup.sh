4145e7c5df2d6a96b99d1027aed4c3151f0d68a6
niynacraft <cat@catcraft.stars.ne.jp>
2024-02-14 16:05:37 +0900

    dd

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 current_user=$(whoami)
 
-mkdir ~/script
-mkdir ~/.steam/
-mkdir ~/.steam/sdk64/
-sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
-steamcmd +login anonymous +app_update 2394010 validate +quit
+#mkdir ~/script
+#mkdir ~/.steam/
+#mkdir ~/.steam/sdk64/
+#sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
+#steamcmd +login anonymous +app_update 2394010 validate +quit
 
 service_file="~/script/palworld-server.service"
 sed -i "s/<user-name>/$current_user/g" $service_file
+
+cat ~/script/palworld-server.service
