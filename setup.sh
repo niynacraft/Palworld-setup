9d77f65cf0ef18878585a5f044471348a434507d
niynacraft <cat@catcraft.stars.ne.jp>
2024-02-14 16:58:30 +0900

    aa

--- setup.sh
+++ setup.sh
@@ -2,23 +2,23 @@ home_directory=~
 current_user=$(whoami)
 
 #ゲーム本体のインストール
-#mkdir ~/script
-#mkdir ~/GameBackupData
-#mkdir ~/.steam/
-#mkdir ~/.steam/sdk64/
-#sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
-#steamcmd +login anonymous +app_update 2394010 validate +quit
+mkdir ~/script
+mkdir ~/GameBackupData
+mkdir ~/.steam/
+mkdir ~/.steam/sdk64/
+sudo add-apt-repository multiverse; sudo dpkg --add-architecture i386; sudo apt update; sudo apt install steamcmd -y
+steamcmd +login anonymous +app_update 2394010 validate +quit
 
-#cp -r ~/Steam/steamapps/common/PalServer/linux64/steamclient.so ~/.steam/sdk64/
+cp -r ~/Steam/steamapps/common/PalServer/linux64/steamclient.so ~/.steam/sdk64/
 
 #スクリプトファイルのコピー
-#cp ~/Palworld-setup/files/start.sh ~/script
-#cp ~/Palworld-setup/files/backup.sh ~/script
-#cp ~/Palworld-setup/files/update.sh ~/script
-#sudo chmod 700 ~/script/*
+cp ~/Palworld-setup/files/start.sh ~/script
+cp ~/Palworld-setup/files/backup.sh ~/script
+cp ~/Palworld-setup/files/update.sh ~/script
+sudo chmod 700 ~/script/*
 
 #サービス化
-#service_file="$home_directory/Palworld-setup/files/palworld-server.service"
+service_file="$home_directory/Palworld-setup/files/palworld-server.service"
 
 echo "Fileの書き換え"
 sed -i "s/<user-name>/$current_user/g" "$home_directory/Palworld-setup/files/palworld-server.service"
